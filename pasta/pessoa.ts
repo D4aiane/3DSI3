9f456c81fc14c9e63fa614d18fdbc38e5d04d09b
D4aiane <dayanesantos200067@gmail.com>
2020-02-08 01:14:57 +0000

    criando classe pessoa

---
+++ pasta/pessoa.ts
@@ -0,0 +1,38 @@
+namespace empresa{
+    class Pessoa{
+        //informações da classe- definir o que a pessoa/objeto tem!
+        private nome:string;
+        private cpf:string;
+        private idade:number | undefined;
+
+
+        // é obrigado a mostrar o cpf  e nome
+        constructor( nome:string, cpf:string){
+            this.cpf = cpf;
+            this.nome = nome;
+        }
+
+        getCpf():string{
+            return this.cpf;
+
+        }
+
+        //pega/mostra o valor do atributo
+        getNome():string |undefined{
+            return this.nome;
+        }
+
+        // altera o valor
+        setNome(nome:string):void{
+            this.nome = nome;
+        }
+
+        getIdade():number | undefined{
+            return this.idade;
+        }
+
+        setIdade(idade:number):void{
+            this.idade = idade;
+        }
+    }
+}
