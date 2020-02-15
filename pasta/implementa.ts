41f126df5137e7981095d585c39b162a3a698e5a
D4aiane <dayanesantos200067@gmail.com>
2020-02-15 01:47:17 +0000

    adicionando funcionario e gerente

--- pasta/implementa.ts
+++ pasta/implementa.ts
@@ -6,4 +6,18 @@ namespace empresa{
     document.getElementById("cpf").textContent = p.getCpf();
     document.getElementById("idade").textContent = p.getIdade().toString();
 
+    let c = new Cliente("Welligton Orelha","120.120.120-00",120);
+    c.setIdade(50);
+    c.setLimite(3000);
+    c.setSaldo(2000);
+
+     document.getElementById("nomeC").textContent = c.getNome();
+      document.getElementById("cpfC").textContent = c.getCpf();
+       document.getElementById("codC").textContent = c.getCodCliente().toString();
+        document.getElementById("idadeC").textContent = c.getIdade().toString();
+         document.getElementById("limiteC").textContent = "R$" +c.getLimite().toString();
+          document.getElementById("saldoC").textContent = "R$" +c.getSaldo().toString();
+
+
+
 }
